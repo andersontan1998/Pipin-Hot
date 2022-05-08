4d47a5ee4c13e3e7aba63d28425718f57b67dab8
Mohfujul Mohammed <47184815+mohfujulm@users.noreply.github.com>
2022-05-08 01:25:21 -0400

    added menu management page for chef, allowing addition of items

---
+++ menu/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import *
+  
+class ImageForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        fields = ['caption', 'img']
+
+class FoodForm(forms.ModelForm):
+    class Meta:
+        model = FoodItem
+        fields = ['name', 'price', 'img', 'category']
--- pipin_hot/settings.py
+++ pipin_hot/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'reg_log'
-
+    'reg_log',
+    'menu',
 ]
 
 MIDDLEWARE = [
@@ -118,9 +119,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
