1b3472f8f89de74292a4adad50cdb3200b8b264c
JohnnyBas1 <82596466+JohnnyBas1@users.noreply.github.com>
2022-05-09 20:46:46 -0400

    order system 2

--- order_system/urls.py
+++ order_system/urls.py
@@ -3,5 +3,7 @@ from order_system import views
 
 urlpatterns = [
     path("order", views.Order.as_view(), name='order'),
+    path("order_confirmation/<int:pk>", views.OrderConfirmation.as_view(),
+         name="order_confirmation")
     #path("logout", views.logout_view, name="logout")
 ]
