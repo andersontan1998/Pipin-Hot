e0833b509440c16514bc762fa0e6e2c79e2b897b
vperls000 <53792766+vperls000@users.noreply.github.com>
2022-05-10 17:24:52 -0400

    updated header

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('defaultHome', defaultHome, name='default')
+    path('defaultHome', defaultHome, name='defaultHome'),
+    path('logOutButton', logOutButton, name='logOutButton'),
+    path('ordersNav', ordersNav, name='ordersNav')
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
 
 # Create your views here.
 def defaultHome(request):
-    return render(request, 'index.html')
+    return render(request, 'defaultHome.html')
+
+def logOutButton(request):
+    logout(request)
+    return redirect('login')
+
+def ordersNav(request):
+    return redirect('order')
