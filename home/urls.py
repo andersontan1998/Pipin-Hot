10e5cbaabf3b5d31436cb47a51e4e77712e1dd1f
vperls000 <53792766+vperls000@users.noreply.github.com>
2022-05-14 11:49:36 -0400

    added some home screen updates manually

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,13 @@
+
 from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('defaultHome', defaultHome, name='default')
+    path('defaultHome', defaultHome, name='defaultHome'),
+    path('loggedinHome', loggedinHome, name='loggedinHome'),
+    path('loginNav', loginNav, name='loginNav'),
+    path('ordersNav', ordersNav, name='ordersNav'),
+    path('menuNav', menuNav, name='menuNav'),
+    path('forumNav', forumNav, name='forumNav'),
+    path('dashNav', dashNav, name='dashNav')
 ]
