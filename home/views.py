6f69952a5bf1ba18ff3791b6eae9614bbf4661ce
vperls000 <53792766+vperls000@users.noreply.github.com>
2022-05-14 15:40:38 -0400

    started styling registration

--- home/views.py
+++ home/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
 # Create your views here.
 def defaultHome(request):
     return render(request, 'defaultHome.html')
 
+# @login_required(redirect_field_name='defaultHome.html')
+# def loggedinHome(request):
+#     return render(request, 'loggedinHome.html')
+
 @login_required(redirect_field_name='defaultHome.html')
-def loggedinHome(request):
-    return render(request, 'loggedinHome.html')
+def logOut(request):
+    logout(request)
+    return redirect('defaultHome')
 
 def loginNav(request):
     return redirect('login')
+
+def registerNav(request):
+    return redirect('register')
 
 def ordersNav(request):
     return redirect('order')
@@ -19,7 +28,7 @@ def menuNav(request):
     return redirect('customer_menu')
 
 def forumNav(request):
-    return redirect('forum')
+    return redirect('all_orders')
 
 def dashNav(request):
     return redirect('cust_dashboard')
