e350793f543c16b59dcb99fa00732dc8aa1688dc
JohnnyBas1 <82596466+JohnnyBas1@users.noreply.github.com>
2022-05-14 12:57:47 -0400

    added very basic manager dashboard

--- reg_log/admin.py
+++ reg_log/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, Customer, Chef, Deliverer
+from .models import User, Customer, Chef, Deliverer, Manager
 
 admin.site.register(User)
 admin.site.register(Customer)
 admin.site.register(Chef)
 admin.site.register(Deliverer)
+admin.site.register(Manager)
